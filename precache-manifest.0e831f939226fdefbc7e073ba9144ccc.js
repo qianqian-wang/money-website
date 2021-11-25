f1b24de074020dcacc49d629f4dd1e834b379a03
qianqianwang <qianqianwang0112@163.com>
2021-11-25 15:39:30 +0800

    deploy

---
+++ precache-manifest.0e831f939226fdefbc7e073ba9144ccc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "52439cf30b89ca56548a",
+    "url": "/money-website/css/app.e5e439e7.css"
+  },
+  {
+    "revision": "7002e2303fcdd3c0573096c65a3fe970",
+    "url": "/money-website/index.html"
+  },
+  {
+    "revision": "52439cf30b89ca56548a",
+    "url": "/money-website/js/app.c9a04044.js"
+  },
+  {
+    "revision": "8631eda8adca93b8e44c",
+    "url": "/money-website/js/chunk-vendors.bf13f08c.js"
+  },
+  {
+    "revision": "6f94a71a55b472b5d6172ab127074578",
+    "url": "/money-website/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/money-website/robots.txt"
+  }
+]);
